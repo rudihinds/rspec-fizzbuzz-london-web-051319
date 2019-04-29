e0ea998d5aed115a4e359d32dfb0bb1ba560096b
rudi hinds <rudihinds@gmail.com>
2019-04-29 15:14:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
- if num % 3 && 5 == 0 
+ if num % 3 && num % 5 == 0 
    return "FizzBuzz"
  else if num % 5 == 0
    return "Buzz"
